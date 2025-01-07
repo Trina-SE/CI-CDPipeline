7e6a4f96770482f44120ea52471500636f14b392
Trina-SE <sultanatrina4209@gmail.com>
2025-01-07 11:43:37 +0600

    correct input

--- test/app.test.js
+++ test/app.test.js
@@ -5,6 +5,6 @@ describe("GET /", () => {
   it("should return 'Hello, World this is a practice for CICD!'", async () => {
     const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe("Hi trina, World this is a practice for CICD!");
+    expect(response.text).toBe("Hello, World this is a practice for CICD!");
   });
 });
